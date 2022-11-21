0411c79e72cb4ecc9780b74216bf4c9074768fb1
Jérôme Trips <jerome.trips@tutanota.com>
2022-11-21 14:52:08 +0100

    implemented state change

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -1,2 +1,9 @@
-const Todo = function () {return <li></li>};
-export default Todo
+import { v4 as uuidv4 } from 'uuid';
+
+export default class ToDo{
+    constructor(txt){
+        this.id = uuidv4();
+        this.text = txt;
+        this.complete = false;
+    }
+}
--- src/components/todolist.jsx
+++ src/components/todolist.jsx
@@ -1,13 +1,33 @@
-const TodoList = function (){
-    return <ul>
-    <li className="todo"><input type="checkbox"></input>
-    Todo
-    </li>
-    <li className="todo"><input type="checkbox"></input>
-    ToDo 2
-    </li>
-    <li className="todo"><input type="checkbox"></input>
-    ToDo 3
-    </li>
-</ul>};
-export default TodoList
+import React, { useState } from "react";
+import ToDo from "./todo";
+let todo1 = new ToDo("My first todo");
+let todo2 = new ToDo ("My second todo");
+
+export default function TodoList() {
+  const initialTodos = [todo1,todo2];
+  const [todos, setTodos] = useState(initialTodos);
+
+  const handleChange = (elementId)  =>{
+    const newToDo = todos.map((todo) => {
+        if (todo.id === elementId){
+            todo.complete = !todo.complete;
+        }
+        return todo;
+    });
+
+    setTodos(newToDo);
+  }
+
+  return (
+
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          <input type="checkbox" id={todo.id} onChange={()=> {
+            handleChange(todo.id);
+          }}   /> {todo.text}
+        </li>
+      ))}
+    </ul>
+  );
+}
