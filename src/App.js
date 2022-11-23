b85066ee19f034ee9184a19ce88815eab3773fad
Jérôme Trips <jerome.trips@tutanota.com>
2022-11-23 09:31:15 +0100

    commented useless code

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import ToDo from './components/todo'
 const LSKEY = "MyTodoApp"; // Should probably be something unique once this goes public but ill just generate a sha256 String 
 
 
-let todo1 = new ToDo("My first todo");
-let todo2 = new ToDo ("My second todo");
+//let todo1 = new ToDo("My first todo");
+//let todo2 = new ToDo ("My second todo");
 
 function App() {
-  const initialTodos = [todo1,todo2];
+  // const initialTodos = [todo1,todo2];
   const [todos, setTodos] = useState(() => {
     return JSON.parse(localStorage.getItem(LSKEY + ".todos")) || [];
   });
